60b7ae84ffbf2c2b8705a41ef6efac87845bf1b4
ranbojah <cp.zf.salman@gmail.com>
2026-06-06 21:09:19 +0700

    fix upload link image

--- next.config.ts
+++ next.config.ts
@@ -1,36 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "i.pinimg.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: '**',
       },
       {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        port: "",
-        pathname: "/a/**",
-      },
-      {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-        port: "",
-        pathname: "/**",
+        protocol: 'http',
+        hostname: '**',
       },
     ],
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
