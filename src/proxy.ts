317464d5fcd0d8d5633c92e4285cf6e70cf3abd3
ranbojah <cp.zf.salman@gmail.com>
2026-06-08 15:15:55 +0700

    proxt

--- src/proxy.ts
+++ src/proxy.ts
@@ -10,12 +10,10 @@ export default withAuth(
 
     const isManualVisit = searchParams.get('mode') === 'public'
 
-    // Redirect jika admin mengakses root '/'
     if (pathname === '/' && token?.roleId === 1 && !isManualVisit) {
       return NextResponse.redirect(new URL('/admin', req.url))
     }
 
-    // Blokir jika user non-admin mencoba mengakses halaman /admin
     if (pathname.startsWith('/admin') && token?.roleId !== 1) {
       return NextResponse.redirect(new URL('/forbidden', req.url))
     }
@@ -27,12 +25,6 @@ export default withAuth(
       authorized: ({ token, req }) => {
         const { pathname } = req.nextUrl
 
-        // PROTEKSI API: Jika mencoba hit API statistik admin tapi bukan admin (roleId !== 1)
-        if (pathname.startsWith('/api/stats') && token?.roleId !== 1) {
-          return false // Mengembalikan HTTP 401/403 otomatis
-        }
-
-        // Proteksi API umum jika tidak login
         if (pathname.startsWith('/api') && !token) {
           return false
         }
@@ -47,6 +39,9 @@ export default withAuth(
 )
 
 export const config = {
+  // matcher: [
+  //   '/((?!api/auth|login|_next/static|_next/image|favicon.ico|public|images).*)',
+  // ],
   matcher: [
     '/admin/:path*',
     '/profile/:path*',
@@ -54,6 +49,5 @@ export const config = {
     '/reports/:path*',
     '/about/:path*',
     '/',
-    '/api/stats',
   ],
 }
